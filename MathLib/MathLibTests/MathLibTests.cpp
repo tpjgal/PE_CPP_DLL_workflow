a79d6dcca8d7968680918d963d4f2990e6ee5179
tpjgal <esakkiponniah@gmail.com>
2026-08-28 22:32:09 +0530

    unit test coverage

--- MathLib/MathLibTests/MathLibTests.cpp
+++ MathLib/MathLibTests/MathLibTests.cpp
@@ -24,5 +24,30 @@ namespace MathLibTests
 		{
 			Assert::AreEqual(6, Calculator::Multiply(2, 3));
 		}
+
+		TEST_METHOD(Divide_ReturnQuotient)
+		{
+			Assert::AreEqual(2.5, Calculator::Divide(5, 2));
+		}
+
+		TEST_METHOD(Divide_ByZero_ThrowsException)
+		{
+			Assert::ExpectException<std::invalid_argument>([]() {
+				Calculator::Divide(5, 0);
+			});
+		}
+
+		TEST_METHOD(IsPrime_TrueForPrimeNumbers)
+		{
+			Assert::IsTrue(Calculator::Isprime(7));
+			Assert::IsTrue(Calculator::Isprime(2));
+		}
+
+		TEST_METHOD(IsPrime_FalseForCompositeAndNonPositiveNumbers)
+		{
+			Assert::IsFalse(Calculator::Isprime(1));
+			Assert::IsFalse(Calculator::Isprime(4));
+			Assert::IsFalse(Calculator::Isprime(9));
+		}
 	};
 }
